31328697ca0ccbce2230a5233c9d277fb12861e9
elachaba <elachabaymane995@gmail.com>
2023-11-17 00:24:32 +0100

    my balls are hot

--- src/Balls.java
+++ src/Balls.java
@@ -1,75 +1,95 @@
 import java.awt.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 import java.lang.StringBuilder;
+
+
 public class Balls {
-    private ArrayList<Point> balls;
+    private ArrayList<Point> positions;
+    private final ArrayList<Point> initPositions = new ArrayList<>();
+    private ArrayList<Point> velocities;
+    private final ArrayList<Point> initVelocities = new ArrayList<>();
 
-    private ArrayList<Point> initialPositions;
-
-    private ArrayList<Point> directions = new ArrayList<>();
-
-
-
-    // Constructeur sans arguments
     public Balls() {
-        this.balls = new ArrayList<>();
-        this.initialPositions = new ArrayList<>();
-
+        this.positions = new ArrayList<>();
+        this.velocities = new ArrayList<>();
     }
 
-
-    public void translate(int dx, int dy, int windowWidth, int windowHeight) {
-        for (int i = 0; i < this.balls.size(); i++) {
-            Point ball = this.balls.get(i);
-            Point direction = this.directions.get(i);
-            int newX = ball.x + dx * direction.x;
-            int newY = ball.y + dy * direction.y;
-
-            // Vérifier les bords horizontaux
-            if (newX < 0) {
-                direction.x = 1;
-                newX = 0; // Set the ball exactly at the edge
-            } else if (newX > windowWidth) {
-                direction.x = -1;
-                newX = windowWidth; // Set the ball exactly at the edge
-            }
-
-            // Vérifier les bords verticaux
-            if (newY < 0) {
-                direction.y = 1;
-                newY = 0; // Set the ball exactly at the edge
-            } else if (newY > windowHeight) {
-                direction.y = -1;
-                newY = windowHeight; // Set the ball exactly at the edge
-            }
-
-            ball.setLocation(newX, newY);
+    public Balls(ArrayList<Point> positions, ArrayList<Point> velocities) {
+        this.positions = positions;
+        this.velocities = velocities;
+        int len = this.positions.size();
+        for (int i = 0; i < len; i++) {
+            this.initPositions.set(i, positions.get(i));
+            this.initVelocities.set(i, velocities.get(i));
         }
     }
 
-    void addBall(int x,int y) {
-        balls.add(new Point(x, y));
-        initialPositions.add(new Point(x, y));
-        directions.add(new Point(1, 1)); // Initial direction: vers le bas et à droite
+    public ArrayList<Point> getPositions() {
+        return positions;
+    }
+
+    public ArrayList<Point> getVelocities() {
+        return velocities;
+    }
+
+    public ArrayList<Point> getInitialPositions() {
+        return initPositions;
+    }
+
+    public ArrayList<Point> getInitVelocities() {
+        return initVelocities;
+    }
+
+    public void setPositions(ArrayList<Point> positions) {
+        this.positions = positions;
+    }
+
+    public void setVelocities(ArrayList<Point> velocities) {
+        this.velocities = velocities;
+    }
+
+    protected void handleRebound(int idx, int windowWidth, int widnowHeight) {
+        Point p = this.positions.get(idx);
+        int vx = this.velocities.get(idx).x;
+        int vy = this.velocities.get(idx).y;
+        if (p.x >= windowWidth || p.x < 0) {
+            vx *= -1;
+        }
+        if (p.y >= widnowHeight || p.y < 0) {
+            vy *= -1;
+        }
+        velocities.get(idx).setLocation(vx, vy);
+    }
+
+    public void translate(int dx, int dy, int windowWidth, int windowHeight) {
+        int len = this.positions.size();
+        for (int i = 0; i < len; i++) {
+            handleRebound(i, windowWidth, windowHeight);
+            int newX = dx + velocities.get(i).x;
+            int newY = dy + velocities.get(i).y;
+
+            positions.get(i).setLocation(newX, newY);
+        }
+    }
+
+    void addBall(int x, int y) {
+        positions.add(new Point(x, y));
+        initPositions.add(new Point(x, y));
+        velocities.add(new Point(1, 1)); // Initial velocity vector is towards the
+        initVelocities.add(new Point(1, 1));
 
     }
     public void reInit() {
-        for (int i = 0; i < balls.size(); i++) {
-            balls.get(i).setLocation(initialPositions.get(i));
-            directions.get(i).setLocation(new Point(1,1));
-
+        for (int i = 0; i < positions.size(); i++) {
+            positions.get(i).setLocation(initPositions.get(i));
+            velocities.get(i).setLocation(initVelocities.get(i));
         }
-    }
-    public List<Point> getBalls() {
-        return this.balls;
     }
 
     @Override
     public String toString(){
         StringBuilder st = new StringBuilder();
-        for (Point ball:balls){
+        for (Point ball: positions) {
             st.append("(").append(ball.x).append(",").append(ball.y).append(")");
         }
         return st.toString();
--- src/BallsSimulator.java
+++ src/BallsSimulator.java
@@ -1,56 +1,52 @@
 import gui.GUISimulator;
 import gui.Oval;
 import gui.Simulable;
-
 import java.awt.*;
 
+
 public class BallsSimulator implements Simulable {
-    private Balls balls;
-
+    private final Balls balls;
     private GUISimulator gui;
+    private final EventManager eventManager;
 
-    private EventManager eventManager;
-    public BallsSimulator(){
+    public BallsSimulator() {
 
         this.balls = new Balls();
         this.eventManager = new EventManager();
         this.eventManager.addEvent(new BallsEvent(1, balls, eventManager, gui));
 
     }
+
     public BallsSimulator(GUISimulator gui) {
         this.balls = new Balls();
         this.eventManager = new EventManager();
         this.eventManager.addEvent(new BallsEvent(1, balls, eventManager, gui));
-
         this.gui = gui;
-        System.out.println(balls);
-
     }
-    public Balls getBalls(){
+
+    public Balls getBalls() {
         return this.balls;
     }
 
-
     @Override
-    public void next(){
+    public void next() {
         this.eventManager.next();
         drawBalls();
     }
+
     @Override
-    public void restart(){
+    public void restart() {
         balls.reInit();
         this.eventManager.restart();
         this.eventManager.addEvent(new BallsEvent(1, balls, eventManager, gui));
         drawBalls();
     }
 
-    public void drawBalls(){
+    public void drawBalls() {
         gui.reset();
-        for (Point ball:balls.getBalls()){
+        for (Point ball:balls.getPositions()) {
             gui.addGraphicalElement(new Oval(ball.x, ball.y, Color.WHITE, Color.WHITE, 10));
 
         }
-
     }
-
 }
